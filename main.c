974136d554bfdd0f3c171abd5f5e73963b439dbc
Matthew Priskorn <mcprisk@umich.edu>
2022-11-18 14:00:49 -0500

    Organized into multiple files

--- main.c
+++ main.c
@@ -21,50 +21,14 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "controller.h"
+#include <string.h>
+#include <stdlib.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
-typedef struct Commands {
-	uint8_t UART_Data;
-	/*
-	 * The full description is used in the CLI and also
-	 * displayed on the touchscreen.
-	*/
-	char full_desc[16];
-	/*
-	 * The shortened description is used on the button array displays.
-	*/
-	char desc[5];
-} Command;
 
-typedef struct Devices {
-	/*
-	 * Commands contain the UART_Data to be sent to devices
-	 * Command 0 is the power toggle
-	 * Commands 1-9 are accessed on the button array
-	 * Commands 10-15 are on the first page of the touchscreen
-	 * Commands 16-21 are on the second page
-	 * Commands 22-27 on the third page
-	*/
-	Command commands[28];
-	/*
-	 * The devices name is printed at the top of the touchscreen
-	 * and is also used to identify devices in the CLI
-	*/
-	char name[32];
-	/*
-	 * The number of buttons associated with this device
-	*/
-	uint8_t num_button_commands;
-	uint8_t num_touch_commands;
-} Device;
-
-typedef struct Controllers {
-	Device devices[16];
-	uint8_t num_devices;
-} Controller;
 
 /* USER CODE END PTD */
 
@@ -79,11 +43,9 @@ typedef struct Controllers {
 
 /* Private variables ---------------------------------------------------------*/
 UART_HandleTypeDef huart1;
-
 /* USER CODE BEGIN PV */
-const char* deliminator = "\n";
-const char* section_deliminator = ",";
-char UART1_rxBuffer[8192] = {'\0'};
+Controller controller;
+unsigned char UART1_rxBuffer[8192] = {'\0'};
 uint8_t sync = 0;
 /* USER CODE END PV */
 
@@ -92,15 +54,6 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART1_UART_Init(void);
 /* USER CODE BEGIN PFP */
-void print_device(uint8_t device, Controller* controller);
-void print_devices(Controller* controller);
-void print_command(uint8_t device, uint8_t command, Controller* controller);
-void print_commands(uint8_t device, Controller* controller);
-void swap_device(uint8_t a, uint8_t b, Controller* controller);
-void rename_device(char* new_name, uint8_t device, Controller* controller);
-void add_device(Controller* controller);
-void delete_device(uint8_t device, Controller* controller);
-void export_configuration(Controller* controller);
 
 /* USER CODE END PFP */
 
@@ -116,45 +69,8 @@ void export_configuration(Controller* controller);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-	Controller controller;
-	controller.num_devices = 0;
 	// TODO: Restore Saved Devices
-
-	// Initial Case: No saved devices
-	if (!controller.num_devices) {
-		strcpy(controller.devices[0].commands[0].desc, "PWR");
-		strcpy(controller.devices[0].commands[0].full_desc, "Power Toggle");
-		controller.devices[0].commands[0].UART_Data = 0x7B;
-		char buf[3];
-		for (uint8_t i = 1; i <= 9; ++i) {
-			itoa(i < 5 ? i : i + 5,buf,10);
-			strcpy(controller.devices[0].name, "Example Device 0");
-			strcpy(controller.devices[0].commands[i < 5 ? i : i + 5].desc, "CM");
-			strncat(controller.devices[0].commands[(i < 5 ? i : i + 5)].desc, buf,strlen(buf));
-			strcpy(controller.devices[0].commands[i < 5 ? i : i + 5].full_desc, "Long Desc ");
-			strncat(controller.devices[0].commands[(i < 5 ? i : i + 5)].full_desc, buf,strlen(buf));
-			controller.devices[0].commands[i < 5 ? i : i + 5].UART_Data = i < 5 ? i : i + 5;
-		}
-		controller.num_devices = 1;
-		controller.devices[0].num_button_commands = 5;
-		controller.devices[0].num_touch_commands = 5;
-
-		strcpy(controller.devices[1].commands[0].desc, "PWR");
-		strcpy(controller.devices[1].commands[0].full_desc, "Power Toggle");
-		controller.devices[1].commands[0].UART_Data = 0x4A;
-		for (uint8_t i = 1; i <= 8; ++i) {
-			itoa(i < 5 ? i : i + 5,buf,10);
-			strcpy(controller.devices[1].name, "Example Device 1");
-			strcpy(controller.devices[1].commands[i < 5 ? i : i + 5].desc, "CM");
-			strncat(controller.devices[1].commands[(i < 5 ? i : i + 5)].desc, buf,strlen(buf));
-			strcpy(controller.devices[1].commands[i < 5 ? i : i + 5].full_desc, "Long Desc ");
-			strncat(controller.devices[1].commands[(i < 5 ? i : i + 5)].full_desc, buf,strlen(buf));
-			controller.devices[1].commands[i < 5 ? i : i + 5].UART_Data = i < 5 ? i : i + 5;
-		}
-		controller.num_devices = 2;
-		controller.devices[1].num_button_commands = 5;
-		controller.devices[1].num_touch_commands = 4;
-	}
+	init_default_controller(&controller);
 
   /* USER CODE END 1 */
 
@@ -178,8 +94,6 @@ int main(void)
   MX_GPIO_Init();
   MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
-  char start_message[] = "Successfully Booted.";
-  HAL_UART_Transmit(&huart1, (uint8_t *)start_message, strlen(start_message), 100);
   HAL_UART_Receive_IT(&huart1, UART1_rxBuffer, 1);
   /* USER CODE END 2 */
 
@@ -326,179 +240,22 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 
     UART1_rxBuffer[5] = '\0';
 
-    if (!strcmp(UART1_rxBuffer,"quit")) {
+    if (!strcmp((char*)UART1_rxBuffer,"quit")) {
     	sync = 0;
     	HAL_UART_Receive_IT(&huart1, UART1_rxBuffer, 1);
     	return;
-    } else if (!strcmp(UART1_rxBuffer,"econ")) {
+    } else if (!strcmp((char*)UART1_rxBuffer,"econ")) {
     	// If the host wants to export, client needs to import
     	//import_configuration();
-    } else if (!strcmp(UART1_rxBuffer,"icon")) {
+    } else if (!strcmp((char*)UART1_rxBuffer,"icon")) {
     	// If the host wants to import, client needs to export
-    	//export_configuration();
-    	char message[] = "I just sent a big packet!";
-    	HAL_UART_Transmit(&huart1, (uint8_t *)message, strlen(message), 100);
+    	export_configuration(huart, &controller);
+    	// char message[] = "I just sent a big packet!";
+    	// HAL_UART_Transmit(&huart1, (uint8_t *)message, strlen(message), 100);
     }
 
     HAL_UART_Receive_IT(huart, UART1_rxBuffer, 4);
 }
-
-void print_number(uint8_t n) {
-	char buf[3];
-	itoa(n,buf,10);
-	HAL_UART_Transmit(&huart1, (uint8_t*)buf, 1, 0xFFFF);
-	HAL_UART_Transmit(&huart1, (uint8_t*)deliminator, 1, 0xFFFF);
-}
-
-void print_device(uint8_t device, Controller* controller) {
-	HAL_UART_Transmit(&huart1, (uint8_t*)(controller->devices[device].name),strlen(controller->devices[device].name),0xFFFF);
-	HAL_UART_Transmit(&huart1, (uint8_t*)deliminator, 1, 0xFFFF);
-}
-
-void print_devices(Controller* controller) {
-	for (uint8_t i = 0; i < controller->num_devices; ++i) {
-		PrintDevice(i,controller);
-	}
-}
-
-void print_command(uint8_t device, uint8_t command, Controller* controller) {
-	char data_buffer[3];
-	itoa(controller->devices[device].commands[command].UART_Data, data_buffer, 16);
-
-	HAL_UART_Transmit(&huart1, (uint8_t*)(controller->devices[device].commands[command].full_desc),strlen(controller->devices[device].commands[command].full_desc),0xFFFF);
-	HAL_UART_Transmit(&huart1, (uint8_t*)section_deliminator, 1, 0xFFFF);
-	HAL_UART_Transmit(&huart1, (uint8_t*)(controller->devices[device].commands[command].desc),strlen(controller->devices[device].commands[command].desc),0xFFFF);
-	HAL_UART_Transmit(&huart1, (uint8_t*)section_deliminator, 1, 0xFFFF);
-	HAL_UART_Transmit(&huart1, (uint8_t*)data_buffer,strlen(data_buffer),0xFFFF);
-	HAL_UART_Transmit(&huart1, (uint8_t*)deliminator, 1, 0xFFFF);
-}
-
-void print_button_commands(uint8_t device, Controller* controller) {
-	for (uint8_t i = 0; i < controller->devices[device].num_button_commands; ++i) {
-		print_command(device, i, controller);
-	}
-}
-
-void print_touch_commands(uint8_t device, Controller* controller) {
-	for (uint8_t i = 10; i < 10 + controller->devices[device].num_touch_commands; ++i) {
-		print_command(device, i, controller);
-	}
-}
-
-void print_commands(uint8_t device, Controller* controller) {
-	print_button_commands(device, controller);
-	print_touch_commands(device, controller);
-}
-
-void add_device(Controller* controller) {
-	char buf[3];
-	char msg[20];
-	itoa(controller->num_devices,buf,10);
-	strcpy(msg, "Device ");
-	strncat(msg, buf, strlen(buf));
-	strcpy(controller->devices[controller->num_devices].name,msg);
-	controller->devices[controller->num_devices].num_button_commands = 0;
-	controller->devices[controller->num_devices].num_touch_commands = 0;
-	++(controller->num_devices);
-}
-
-void delete_device(uint8_t device, Controller* controller) {
-	--(controller->num_devices);
-	for (uint8_t i = device; i < controller->num_devices; ++i) {
-		swap_device(i, i+1, controller);
-	}
-}
-
-/*
- * Device Export Format:
- * Device Name
- * Number of Button Commands (including pwr)
- * Command #1 Long Name, Short Name, Data
- * Command #2 Long Name, Short Name, Data
- * ...
- * Number of Touch Commands
- * Command #1 Long Name, Short Name, Data
- * Command #2 Long Name, Short Name, Data
- */
-void export_device(uint8_t device, Controller* controller) {
-	print_device(device, controller);
-
-	print_number(controller->devices[device].num_button_commands);
-	print_button_commands(device, controller);
-
-	print_number(controller->devices[device].num_touch_commands);
-	print_touch_commands(device, controller);
-}
-/*
- * Device Import Format:
- * Device Name
- * Number of Button Commands (including pwr)
- * Command #1 Long Name, Short Name, Data
- * Command #2 Long Name, Short Name, Data
- * ...
- * Number of Touch Commands
- * Command #1 Long Name, Short Name, Data
- * Command #2 Long Name, Short Name, Data
- */
-void import_device();
-
-void rename_device(char* new_name, uint8_t device, Controller* controller) {
-	strcpy(controller->devices[device].name, new_name);
-}
-void swap_device(uint8_t a, uint8_t b, Controller* controller) {
-	Device tmp = controller->devices[a];
-	controller->devices[a] = controller->devices[b];
-	controller->devices[b] = tmp;
-}
-
-void add_command();
-void set_command();
-void delete_command();
-void rename_command();
-void swap_command();
-void copy_command();
-
-/*
- * Configuration Export Format:
- * # of Devices
- * Device #1 Name
- * Number of Button Commands (including pwr)
- * Command #1 Long Name, Short Name, Data
- * Command #2 Long Name, Short Name, Data
- * ...
- * Number of Touch Commands
- * Command #1 Long Name, Short Name, Data
- * Command #2 Long Name, Short Name, Data
- * (blank line)
- * Device #2 Name
- * ...
-*/
-void export_configuration(Controller* controller) {
-
-	print_number(controller->num_devices);
-
-	for (uint8_t i = 0; i < controller->num_devices; ++i) {
-		export_device(i, controller);
-		HAL_UART_Transmit(&huart1, (uint8_t*)deliminator, 1, 0xFFFF);
-	}
-}
-/*
- * Configuration Export Format:
- * # of Devices
- * Device #1 Name
- * Number of Button Commands (including pwr)
- * Command #1 Long Name, Short Name, Data
- * Command #2 Long Name, Short Name, Data
- * ...
- * Number of Touch Commands
- * Command #1 Long Name, Short Name, Data
- * Command #2 Long Name, Short Name, Data
- * (blank line)
- * Device #2 Name
- * ...
-*/
-void import_configuration();
-
 /* USER CODE END 4 */
 
 /**
